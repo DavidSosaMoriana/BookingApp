43176398e67e911c024815d0895aedb6b6877f18
David Sosa <david.sosa@singularfactory.com>
2022-05-03 22:05:07 +0100

    NavBar Changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,5 @@
 import "./navbar.css"
 
-/**
- * This function returns a div with a class of navbar, which contains a div with a class of
- * navContainer, which contains a span with a class of logo, which contains the text 'davbooking',
- * which contains a div with a class of navItems, which contains a button with a class of navButton,
- * which contains the text 'Register', which contains a button with a class of navButton, which
- * contains the text 'Login'.
- * @returns A React component.
- */
 const Navbar = () => {
   return (
     <div className="navbar">

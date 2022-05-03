5dba3903b760e75a2c3b715fc011bc0599932f77
David Sosa <david.sosa@singularfactory.com>
2022-05-03 21:24:33 +0100

    Header Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import { 
   faBed,
-   
+  faPlane,
+  faCar,
+  faTaxi 
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
@@ -8,12 +10,36 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-        <FontAwesomeIcon icon={faBed} />
-        </div>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+          <FontAwesomeIcon icon={faPlane} />
+          <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+          <FontAwesomeIcon icon={faCar} />
+          <span>Car Rentals</span>
+          </div>
+          <div className="headerListItem">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+          <FontAwesomeIcon icon={faTaxi} />
+          <span>Airport taxis</span>
+          </div>
       </div>
+      <h1 className="headerTitle">A lifetime of discount? it's genius</h1>
+      <p className="headerDesc">
+        Get rewarded for your travels - unlock instant savings of 10% or more with a free Davbooking account
+      </p>
+      <button className="headerBtn">Sign in / Register</button>
     </div>
+  </div>
   )
 }
 
